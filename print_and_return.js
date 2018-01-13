e6ce896f3fc6dc25f31b147c6a24d4cefa0dc1ca
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-01-12 22:01:06 -0500

    Add files via upload

---
+++ print_and_return.js
@@ -0,0 +1,11 @@
+/*Print and Return
+Your function will receive an array with two numbers. 
+Print the first value, and return the second.*/
+
+function print_and_return(x,y){
+    var arr = [];
+    arr.push(x);
+    arr.push(y);
+    console.log(arr);
+}
+print_and_return(1,2);
