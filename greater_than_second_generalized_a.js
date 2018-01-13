4288be3d4278c8680848713cf7453be9de2d4154
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-01-12 22:22:33 -0500

    Add files via upload

---
+++ greater_than_second_generalized_a.js
@@ -0,0 +1,18 @@
+/*Values Greater than Second, Generalized
+Write a function that accepts any array, and returns a 
+new array with the array values that are greater than its 
+2nd value. Print how many values this is. 
+What will you do if the array is only one element long?*/
+
+function valuesSecond(arr){
+    var newarr=[];
+    for(var i = 0; i<arr.length; i++){
+        if(arr[i]>arr[1]){
+            newarr.push(arr[i]);
+        }
+    }
+    console.log(newarr);
+    console.log(newarr.length);
+};
+valuesSecond([1,2,3,4,5]);
+valuesSecond([1]);
