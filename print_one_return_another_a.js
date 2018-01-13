9bec1f1353a07ff2edb6a15eac12ae621c9de2a5
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-01-13 09:05:31 -0500

    Add files via upload

---
+++ print_one_return_another_a.js
@@ -0,0 +1,18 @@
+/*Print One, Return Another
+Build a function that takes array of numbers. 
+The function should print second-to-last value in 
+the array, and return first odd value in the array. */
+
+function printAnother(arr){
+    
+    console.log(arr[arr.length-2]);
+    
+    for(var i = 0; i<arr.length; i++){
+        if(arr[i] % 3 === 0){
+            return arr[i];
+        }
+        break;
+}}
+printAnother([2,3,5,8,9,4]);
+
+
