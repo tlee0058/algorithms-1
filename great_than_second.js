aae76fe290bd6173da661d1fd403d49c7914ce42
tlee0058 <32435667+tlee0058@users.noreply.github.com>
2018-01-15 12:27:34 -0500

    Update great_than_second.js

--- great_than_second.js
+++ great_than_second.js
@@ -6,6 +6,7 @@ function greaterThanSecond(arr){
     var count = 0;
     for(var i = 0; i < arr.length; i++){
         if(arr[i]>arr[1]){
+            console.log(i);
             count += 1;
         }
     }
